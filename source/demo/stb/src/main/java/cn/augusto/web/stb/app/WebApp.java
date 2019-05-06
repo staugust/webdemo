e87e348096eb52f05564e4756b442d38956dcbae
Jinghua.Yao <jinghua.yao@iluvatar.ai>
2019-05-06 18:30:44 +0800

    first version

---
+++ source/demo/stb/src/main/java/cn/augusto/web/stb/app/WebApp.java
@@ -0,0 +1,14 @@
+package cn.augusto.web.stb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+public class WebApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebApp.class, args);
+  }
+}
---
+++ source/demo/stb/src/main/java/cn/augusto/web/stb/controller/LoginController.java
@@ -0,0 +1,17 @@
+package cn.augusto.web.stb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+  @RequestMapping("/")
+  public String index(){
+    return "/index";
+  }
+
+  @RequestMapping("/login/")
+  public String login(){
+    return "/index";
+  }
+}
